d862c5c8e93a259a35f6e9749d353a3331aa99a6
Pushpinder Virdi <pushpindervirdi94@gmail.com>
2020-05-01 01:37:06 -0400

    Adding Database Variable

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const cors = require('cors');
 const db = require('knex')({
 	client : 'pg',
 	connection : {
-		host : '127.0.0.1',
-		user : 'postgres',
-		password : '9266',
-		database : 'howdy'
+		host : process.env.DATABASE_URL,
+		ssl : true
 	}
 });
 
